f29cf4f033416fdee7a286d7528ea3a7481a5be0
display-none <jaczula@gmail.com>
2015-10-25 22:54:34 +0100

    added travis file

---
+++ src/main/java/org/turing/Main.java
@@ -0,0 +1,9 @@
+package org.turing;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/org/turing/entity/IEntity.java
@@ -0,0 +1,6 @@
+package org.turing.entity;
+
+public interface IEntity {
+
+	Long getId();
+}
