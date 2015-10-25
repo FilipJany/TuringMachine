26d33a125dd9de0c3f2f5568ab0676889e43bea8
display-none <jaczula@gmail.com>
2015-10-25 22:54:34 +0100

    the very basics

---
+++ src/test/java/org/turing/entity/EmployeeTest.java
@@ -0,0 +1,13 @@
+package org.turing.entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeTest {
+	
+	@Test
+	public void test() throws Exception {
+		assertEquals("dupa", "dupa");
+	}
+}
