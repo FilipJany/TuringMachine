6ba12b9c49115b6b10d540a559c83dbbe40ff81a
Patryk Stopyra <ps.magnat@op.pl>
2016-01-10 18:23:34 +0100

    Improved application gui constants.

--- src/main/java/org/turing/app/views/constants/ApplicationConstraints.java
+++ src/main/java/org/turing/app/views/constants/ApplicationConstraints.java
@@ -6,10 +6,10 @@ package org.turing.app.views.constants;
 public  class ApplicationConstraints
 {
     //MainFrame
-    public final static int mainFrameMinimalWidth = 770, mainFrameMinimalHeight = 500;
-    public final static int mainFrameStartLocationX = 50, mainFrameStartLocationY = 50;
+    public final static int mainFrameMinimalWidth = 770, mainFrameMinimalHeight = 300;
+    public final static int mainFrameStartLocationX = 30, mainFrameStartLocationY = 30;
     //ControlPanel
-    public final static int minimalControlPanelWidth = 350, minimalControlPanelHeight = 130, buttonHeight = 30, buttonWidth = 80, minimalButtonHeight = 20, minimalButtonWidth = 50;
+    public final static int minimalControlPanelWidth = 350, minimalControlPanelHeight = 80, buttonHeight = 30, buttonWidth = 80, minimalButtonHeight = 20, minimalButtonWidth = 50;
     //SliderPanel
     public final static int sliderMinValue = 0, sliderInitValue = 10, sliderMaxValue = 20, sliderStep = 1;
     public static int sliderPanelMinimalWidth = 350, sliderPanelMinimalHeight = 100;
@@ -18,8 +18,8 @@ public  class ApplicationConstraints
     public final static int minimalTapePanelWidth = 500, minimalTapePanelHeight = 200, textFieldHigh = 30, textFieldWidth = 30;
     public static int TapeSize = 15;
     //ProgramFrame
-    public final static int programFrameMinimalWidth = 600, programFrameMinimalHeight = 400;
-    public final static int programFrameStartLocationX = 840, programFrameStartLocationY = 50;
+    public final static int programFrameMinimalWidth = 600, programFrameMinimalHeight = 300;
+    public final static int programFrameStartLocationX = 810, programFrameStartLocationY = 30;
     //TablePanel
     public final static int tablePanelButtonHeight = 15, tablePanelButtonWidth = 15;
     public final static int tableMinimalHeight = 300, tableMinimalWidth = 500;
